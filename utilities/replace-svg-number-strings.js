fbeeb0279846c73746e78f1c9fb452b838b39b9a
Nano Miratus <nanomiratus@gmail.com>
2023-03-14 02:54:12 +0100

    fix: :bug: fix bug with stroke-width

--- utilities/replace-svg-number-strings.js
+++ utilities/replace-svg-number-strings.js
@@ -3,122 +3,122 @@ import SvgPath from "svgpath";
 
 import { dimensionlessPathCommandParameterIndexes, traverseSvg } from "@ai-flags/utilities";
 
-const replaceSvgNumberStrings = (svg, replaceMap) => {
-	return traverseSvg(
-		svg,
-		(element) => {
-			let { attributes, tagName } = element;
+/**
+ *
+ * @param svg
+ * @param replaceMap
+ */
+const replaceSvgNumberStrings = (svg, replaceMap) => traverseSvg(
+	svg,
+	(element) => {
+		let { attributes, tagName } = element;
 
-			const tagNamesWithStrokeWidth = new Set([
-				"altGlyph",
-				"circle",
-				"ellipse",
-				"g",
-				"line",
-				"path",
-				"polygon",
-				"polyline",
-				"rect",
-				"text",
-				"textPath",
-				"tref",
-				"tspan",
-				"use"
-			]);
+		const tagNamesWithStrokeWidth = new Set([
+			"altGlyph",
+			"circle",
+			"ellipse",
+			"g",
+			"line",
+			"path",
+			"polygon",
+			"polyline",
+			"rect",
+			"text",
+			"textPath",
+			"tref",
+			"tspan",
+			"use"
+		]);
 
-			if (!element.hasAttribute("stroke-width") && tagNamesWithStrokeWidth.has(tagName)) {
-				element.setAttribute("stroke-width", 1);
+		if (!element.hasAttribute("stroke-width") && element.hasAttribute("stroke") && tagNamesWithStrokeWidth.has(tagName)) {
+			element.setAttribute("stroke-width", 1);
 
-				({ attributes } = element);
-			}
+			({ attributes } = element);
+		}
 
-			for (const { name, value } of Array.from(attributes)) {
-				switch (name) {
-					case "d": {
-						const svgPath = new SvgPath(value);
-						const { segments } = svgPath;
+		for (const { name, value } of Array.from(attributes)) {
+			switch (name) {
+				case "d": {
+					const svgPath = new SvgPath(value);
+					const { segments } = svgPath;
 
-						const newSegments = [];
+					const newSegments = [];
 
-						for (const [letter, ...numbers] of segments) {
-							newSegments.push([
-								letter,
-								...numbers.map(
-									(number, index) => dimensionlessPathCommandParameterIndexes.has(letter) && dimensionlessPathCommandParameterIndexes.get(letter).has(index)
-										? number
-										: replaceMap.get(format(bignumber(number), { notation: "fixed" })) || number
+					for (const [letter, ...numbers] of segments) {
+						newSegments.push([
+							letter,
+							...numbers.map(
+								(number, index) => (dimensionlessPathCommandParameterIndexes.has(letter) && dimensionlessPathCommandParameterIndexes.get(letter).has(index)
+									? number
+									: replaceMap.get(format(bignumber(number), { notation: "fixed" })) || number)
+							)
+						]);
+					}
+
+					svgPath.segments = newSegments;
+
+					element.setAttribute(name, svgPath.toString());
+
+					break;
+				}
+
+				case "transform": {
+					const transformFunctions = new Map(
+						value.split(/(?<=\))\s/g)
+							.map((transformFunctionString) => {
+								const [transformFunctionName, ...parameters] = transformFunctionString
+									.trim()
+									.split(/\(|\)/g)
+									.slice(0, -1)
+									.map((parametersString, index) => (index === 0 ? parametersString : parametersString.trim().split(/\s|,/g).map(Number)))
+									.flat();
+
+								return [transformFunctionName, parameters];
+							})
+					);
+
+					const scaledTransformFunctions = new Map(
+						Array.from(transformFunctions)
+							.map(([transformFunctionName, parameters]) => [
+								transformFunctionName,
+								parameters.map(
+									(parameter, index) => {
+										if (
+											(transformFunctionName === "matrix" && index > 3) ||
+											(transformFunctionName === "translate") ||
+											(transformFunctionName === "rotate" && index > 0)
+										) {
+											return replaceMap.get(format(bignumber(parameter), { notation: "fixed" })) || parameter;
+										}
+
+										return parameter;
+									}
 								)
-							]);
-						}
+							])
+					);
 
-						svgPath.segments = newSegments;
+					const newTransformString = Array.from(scaledTransformFunctions)
+						.map(([transformFunctionName, parameters]) => `${transformFunctionName}(${parameters.join(", ")})`)
+						.join(" ");
 
-						element.setAttribute(name, svgPath.toString());
+					element.setAttribute(name, newTransformString);
 
-						break;
-					}
-					case "transform": {
-						const transformFunctions = new Map(
-							value.split(/(?<=\))\s/g)
-								.map((transformFunctionString) => {
-									const [transformFunctionName, ...parameters] = transformFunctionString
-										.trim()
-										.split(/\(|\)/g)
-										.slice(0, -1)
-										.map((parametersString, index) => index === 0 ? parametersString : parametersString.trim().split(/\s|,/g).map(Number))
-										.flat();
+					break;
+				}
 
-									return [transformFunctionName, parameters];
-								})
-						);
+				default: {
+					const newAttributeValue = value.replace(
+						/(?:(?<=(?:^| |\(|,))(?:(?:\d+(?:\.\d+)?)|(?:\d+)?\.\d+)(?=(?:$| |\)|-|\.\d|,)))|(?:(?<=(?:^| |\(|\d|,))-(?:(?:\d+(?:\.\d+)?)|(?:\d+)?\.\d+)(?=(?:$| |\)|-|\.\d|,)))|(?:(?<=(?:^| |\(|\d|,))(?:(?:\d+)?\.\d+)(?=(?:$| |\)|-|\.\d|,)))/g,
+						(match) => replaceMap.get(format(bignumber(match), { notation: "fixed" })) || match
+					);
 
-						const scaledTransformFunctions = new Map(
-							Array.from(transformFunctions)
-								.map(([transformFunctionName, parameters]) => {
-									return [
-										transformFunctionName,
-										parameters.map(
-											(parameter, index) => {
-												if (
-													(transformFunctionName === "matrix" && index > 3) ||
-													(transformFunctionName === "translate") ||
-													(transformFunctionName === "rotate" && index > 0)
-												) {
-													return replaceMap.get(format(bignumber(parameter), { notation: "fixed" })) || parameter
-												}
-												else {
-													return parameter;
-												}
-											}
-										)
-									]
-								})
-						);
+					element.setAttribute(name, newAttributeValue);
 
-						const newTransformString = Array.from(scaledTransformFunctions)
-							.map(([transformFunctionName, parameters]) => `${transformFunctionName}(${parameters.join(", ")})`)
-							.join(" ");
-
-						element.setAttribute(name, newTransformString);
-
-						break;
-					}
-					default: {
-						const newAttributeValue = value.replace(
-							/(?:(?<=(?:^| |\(|,))(?:(?:\d+(?:\.\d+)?)|(?:\d+)?\.\d+)(?=(?:$| |\)|-|\.\d|,)))|(?:(?<=(?:^| |\(|\d|,))-(?:(?:\d+(?:\.\d+)?)|(?:\d+)?\.\d+)(?=(?:$| |\)|-|\.\d|,)))|(?:(?<=(?:^| |\(|\d|,))(?:(?:\d+)?\.\d+)(?=(?:$| |\)|-|\.\d|,)))/g,
-							(match) => {
-								return replaceMap.get(format(bignumber(match), { notation: "fixed" })) || match;
-							}
-						);
-
-						element.setAttribute(name, newAttributeValue);
-
-						break;
-					}
+					break;
 				}
 			}
 		}
-	);
-};
+	}
+);
 
 export default replaceSvgNumberStrings;
